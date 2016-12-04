41faedbd84c8c9f45fd02b93d2ee0d8a35237ad3
Joan Sirma <Jc.sirma@gmail.com>
2016-12-04 15:04:47 -0800

    added Google Maps API

--- app/src/main/java/com/example/steven/baearea/EventListPage.java
+++ app/src/main/java/com/example/steven/baearea/EventListPage.java
@@ -1,7 +1,11 @@
 package com.example.steven.baearea;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class EventListPage extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class EventListPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_page2);
+
+        Button checkAvail = (Button) findViewById(R.id.checkAvail);
+        checkAvail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Clicks", "loading ...");
+                Intent i = new Intent(EventListPage.this, EventPage.class);
+                startActivity(i);
+
+            }
+        });
+
+
+
     }
+
 }
--- app/src/main/java/com/example/steven/baearea/EventPage.java
+++ app/src/main/java/com/example/steven/baearea/EventPage.java
@@ -1,7 +1,11 @@
 package com.example.steven.baearea;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class EventPage extends AppCompatActivity {
     // add events
@@ -10,5 +14,6 @@ public class EventPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+
     }
 }
