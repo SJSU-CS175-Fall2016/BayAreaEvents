1a12778ae5d99de24357ea40d775a44898fd4d3b
Joan Sirma <Jc.sirma@gmail.com>
2016-12-19 23:49:03 -0800

    Working / Croeect Files

---
+++ app/src/main/java/com/example/steven/baearea/EventbriteHelper.java
@@ -0,0 +1,100 @@
+package com.example.steven.baearea;
+
+import android.util.Log;
+
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.util.Log;
+
+/**
+ * Created by Joan on 12/4/16.
+ * Get the public API's from Eventbrite
+ */
+public class EventbriteHelper {
+    // specific event will be replaced in the "id"
+    //search event
+    //private static final String EventURL ="https://www.eventbriteapi.com/v3/events/search/?q=q&token=EHX6AHAT3A5NVBCLVKSA";
+    private static final String EventURL = "https://www.eventbriteapi.com/v3/events/search/?q";
+    
+   // private static final String DateURL = "https://www.eventbriteapi.com/v3/events/search/start_date/?5GWBURM7CJDIIMTMVV";
+
+    private static final int HTTP_STATUS_OK = 200;
+    private static byte[] buff = new byte[1024];
+    private static final String logTag = "Eventbrite";
+
+
+
+    public static class ApiException extends Exception {
+        private static final long serialVersionUID = 1L;
+
+        public ApiException (String msg)
+        {
+            super (msg);
+        }
+
+        public ApiException (String msg, Throwable thr)
+        {
+            super (msg, thr);
+        }
+    }
+
+    protected static synchronized String downloadFromServer (String... searchString)
+            throws ApiException
+    {
+
+        // get events with specific id
+
+        String retval = null;
+        String q= searchString[0];
+        String token = "EHX6AHAT3A5NVBCLVKSA";
+
+
+        String url = EventURL + "=" + q +"&token=" + token;
+        Log.d(logTag,"Fetching " + url);
+        // create an http client and a request object.
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet(url);
+        try {
+
+            // execute the request
+            HttpResponse response = client.execute(request);
+            StatusLine status = response.getStatusLine();
+            if (status.getStatusCode() != HTTP_STATUS_OK) {
+                // handle error here
+                throw new ApiException("Invalid response" +
+                        status.toString());
+            }
+
+            // process the content.
+            HttpEntity entity = response.getEntity();
+            InputStream ist = entity.getContent();
+            ByteArrayOutputStream content = new ByteArrayOutputStream();
+
+            int readCount = 0;
+            while ((readCount = ist.read(buff)) != -1) {
+                content.write(buff, 0, readCount);
+            }
+            retval = new String (content.toByteArray());
+
+        } catch (Exception e) {
+            throw new ApiException("Problem connecting to the server " +
+                    e.getMessage(), e);
+        }
+
+        return retval;
+    }
+}
---
+++ app/src/main/java/com/example/steven/baearea/EventsListActivity.java
@@ -0,0 +1,153 @@
+package com.example.steven.baearea;
+
+import android.app.Activity;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Context;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Joan on 12/5/16.
+ */
+public class EventsListActivity extends Activity {
+
+    private ArrayList<EventData> events;
+    private ListView eventsList;
+    //private AdapterView eventsList;
+    private Spinner locationSpinner;
+    private LayoutInflater layoutInflator;
+    private Button searchButton;
+    private EventbriteIconTask imgFetcher;
+    private InputMethodManager inMgr;
+    private DatabaseReference mDatabase;
+    private Button mSaveButton;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.eventsactivity);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mSaveButton = (Button) findViewById(R.id.saveButton);
+
+
+        // this.inMgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        this.locationSpinner = (Spinner) findViewById(R.id.location_spinner);
+        this.searchButton= (Button) findViewById(R.id.search_button);
+        this.imgFetcher = new EventbriteIconTask(); // add (this)
+        this.layoutInflator =  LayoutInflater.from(this);
+        this.eventsList = (ListView) findViewById(R.id.event_list);
+
+
+        this.searchButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //inMgr.hideSoftInputFromWindow(searchButton.getWindowToken(), 0);
+                EventbriteWebAPITask evtTask = new EventbriteWebAPITask(EventsListActivity.this);
+                try {
+                    //create a task that finds events for specified locations
+                    TextView txtView  =  (TextView)locationSpinner.getSelectedView();
+                    String metroTxt = txtView.getText().toString();
+                    evtTask.execute(metroTxt);
+//                    Intent i = new Intent(EventsListActivity.this, EventListAdapter.class);
+//                    startActivity(i);
+                }
+                catch (Exception e)
+                {
+                    evtTask.cancel(true);
+                    alert (getResources().getString(R.string.no_events));
+                }
+
+            }
+        });
+
+        this.mSaveButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                try{
+                    //String eventName = JSONObject.getString("name");
+                    mDatabase.child("Eventname").setValue("1");
+                }
+                catch (Exception e)
+                {
+                    alert("Unable to save");
+                }
+            }
+        });
+
+        final Object[] data = (Object[]) getLastNonConfigurationInstance();
+
+        if(data != null) {
+            this.events = (ArrayList<EventData>) data[0];
+            this.imgFetcher = (EventbriteIconTask)data[1];
+            eventsList.setAdapter(new EventListAdapter(this,this.imgFetcher,this.layoutInflator, this.events));
+        }
+
+        // pre populates the spinner with cities
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Location, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        locationSpinner.setAdapter(adapter);
+
+        }
+
+
+    public static class MyViewHolder {
+        public TextView eventName, eventDate;
+        public Button searchButton;
+        public ImageView icon;
+        public EventData event;
+    }
+
+
+    public void alert (String msg)
+    {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+
+
+
+    public void setEvents(ArrayList<EventData> events) {
+        System.out.println("6");
+        //saves the event array as local obj
+        this.events = events;
+        this.eventsList.setAdapter(new EventListAdapter (this, this.imgFetcher,this.layoutInflator, this.events));
+
+
+        System.out.println(eventsList);
+        System.out.println("7");
+    }
+
+
+
+    }
+
+
+
